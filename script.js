f800133c73000941c66df89c5c9e504691b03c2f
Enike Charan Kumar <94379689+charanenike@users.noreply.github.com>
2023-10-11 06:40:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 // Data
 const account1 = {
-  owner: 'Charan Kumar',
+  owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
